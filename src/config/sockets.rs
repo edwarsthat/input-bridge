c026ff607312fb1ced1f650a7c1208709f613df8
edwarsthat <edwarsthat@gmail.com>
2026-05-01 09:05:26 -0500

    se agrego la ip

--- src/config/sockets.rs
+++ src/config/sockets.rs
@@ -1,5 +1,5 @@
-pub const HOST_IP: &str = "0.0.0.0";
+pub const HOST_IP: &str = "192.168.0.14";
 pub const HOST_PORT: u16 = 5000;
 
-pub const CLIENT_IP: &str = "0.0.0.0";
+pub const CLIENT_IP: &str = "192.168.0.19";
 pub const CLIENT_PORT: u16 = 0;
